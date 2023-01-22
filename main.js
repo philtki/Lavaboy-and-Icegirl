b58e8e7aeb44b8839c4390b252c239aacb5dde8c
Nathan Mahnke <nmahnke@uw.edu>
2023-01-21 21:56:36 -0800

    Added animation for fireboy's idle pose

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./Assets/background.png");
+ASSET_MANAGER.queueDownload("./Assets/Fireboy/SpriteSheet.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.addEntity(new background());
+	// gameEngine.addEntity(new background());
+	gameEngine.addEntity(new fireboy(gameEngine));
 
 	gameEngine.init(ctx);
 
