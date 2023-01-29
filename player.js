1fa926532537133024300fa8ba47badf88715d4e
Nathan9819 <109249606+Nathan9819@users.noreply.github.com>
2023-01-28 16:24:10 -0800

    Update player.js
    fixed fireboy animations

--- player.js
+++ player.js
@@ -1,6 +1,7 @@
 class player {
     constructor (game, isIceGirl) {
         this.game = game;
+        this.isIceGirl = isIceGirl
         this.state = 0; // 0 = idle, 1 = falling, 2 = jumping, 3 = running
         this.moving = 0; // 0 = idle, 1 = left, 2 = right
         this.dead = false;
@@ -12,6 +13,7 @@ class player {
     }
 
     loadAnimations() {
+        if (!this.isIceGirl) {
         // Fireboy Idle
         this.animations[0] = new animator(ASSET_MANAGER.getAsset("./Assets/Fireboy/SpriteSheet.png"), 0, 410, 214, 410, 5, 0.08);
         // Fireboy Falling
@@ -20,36 +22,62 @@ class player {
         this.animations[2] = new animator(ASSET_MANAGER.getAsset("./Assets/Fireboy/SpriteSheet.png"), 0, 820, 214, 410, 5, 0.08);
         // Fireboy Running Right
         this.animations[3] = new animator(ASSET_MANAGER.getAsset("./Assets/Fireboy/SpriteSheet.png"), 0, 1230, 341, 270, 8, 0.06);
+        } else {
+
+        }
     }
 
     update() {
         // Running right
         if (this.game.FBRight && !this.game.FBLeft) {
             this.state = 3;
-            this.moving = 2;
+            this.moving = 1;
         // Running Left
         } else if (this.game.FBLeft && !this.game.FBRight) {
             this.state = 3;
-            this.moving = 1;
+            this.moving = 2;
+        }
+
         // Jumping
-        } else if (this.game.FBUp && !this.game.FBDown && !this.game.FBLeft && !this.game.FBRight) {
+        if (this.game.FBUp) {
             this.state = 2;
-            this.moving = 0;
         // Falling
-        } else if (this.game.FBDown && !this.game.FBUp && !this.game.FBLeft && !this.game.FBRight) {
+        } else if (this.game.FBDown) {
             this.state = 1;
-            this.moving = 0;
         // Idle
-        } else {
+        } else if (((!this.game.FBLeft && !this.game.FBRight) || (this.game.FBLeft && this.game.FBRight)) && !this.game.FBUp) {
             this.state = 0;
             this.moving = 0;
         }
+        // console.log("State is: " + this.state);
+        // console.log("Moving is: " + this.moving);
     };
 
 
     draw(ctx) {
- 
-        this.animations[this.state].drawFrame(this.game.clockTick, ctx, 500, 500, .3);
+        this.Xoffset = 0;
+        this.Yoffset = 0;
+
+        if (this.state == 3 && this.moving == 1) {
+            this.Xoffset = -43;
+            this.Yoffset = 42;
+        } else if (this.state == 3 && this.moving == 2) {
+            this.Xoffset = 105;
+            this.Yoffset = 42;
+        }
+        
+        if (this.moving == 0 || this.moving == 1) {
+        this.animations[this.state].drawFrame(this.game.clockTick, ctx, 500 + this.Xoffset, 500 + this.Yoffset, .3);
+        } else {
+            ctx.save();
+            ctx.scale(-1, 1);
+            this.animations[this.state].drawFrame(this.game.clockTick, ctx, -500 - this.Xoffset, 500 + this.Yoffset, .3);
+            ctx.restore();
+        }
+
+        // this.animations[0].drawFrame(this.game.clockTick, ctx, 500, 500, .3);
+        // this.animations[3].drawFrame(this.game.clockTick, ctx, 500  - 43, 500 + 42, .3);
+
         // ctx.drawImage(ASSET_MANAGER.getAsset("./Assets/Fireboy/SpriteSheet.png"), 0, 0);
     }
 }
