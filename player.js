f8e942a6cc83ecf3d1016c5e3d421d700b9af367
Nathan Mahnke <nmahnke@uw.edu>
2023-02-05 20:28:10 -0800

    added jumping

--- player.js
+++ player.js
@@ -9,13 +9,14 @@ class player {
         this.dead = false;
         this.velocity = { x: 0, y: 0 };
         this.fallAcc = 562.5;
+        this.grounded = true;
         this.animations = [];
         this.loadAnimations();
         if (this.isIceGirl) {
-            this.BB = new boundingbox(this.x + 10, this.y + 15, 40, 90, "Yellow");
+            this.BB = new boundingbox(this.x + 10, this.y + 10, 40, 90, "Yellow");
             this.name = "IG";
         } else {
-            this.BB = new boundingbox(this.x + 10, this.y + 15, 40, 90, "Yellow");
+            this.BB = new boundingbox(this.x + 10, this.y + 10, 40, 90, "Yellow");
             this.name = "FB";
         }
         this.lastBB = this.BB;
@@ -51,18 +52,24 @@ class player {
         const MAX_RUN = 150;
         const ACC_RUN = 200;
         const DEC_RUN = 40;
+        const DEC_AIR = 40;
         const MAX_FALL = 270;
+        const MAX_JUMP = 300;
 
         // Running right
         eval("this.left = this.game." + this.name + "Left;");
         eval("this.right = this.game." + this.name + "Right;");
         eval("this.up = this.game." + this.name + "Up;");
         eval("this.down = this.game." + this.name + "Down;");
+        
+        if (this.grounded) {
+            this.velocity.y = 0;
+        }
+        
         if(this.left && this.right) {
             this.state = 0;
             this.moving = 0;
         } else if (this.right) {
-            console.log("hello");
             this.state = 3;
             this.moving = 1;
             this.velocity.x = MAX_RUN;
@@ -75,15 +82,20 @@ class player {
 
         // Jumping
         if (this.up) {
-            this.state = 2;
-        // Falling
-        } else if (this.down) {
-            this.state = 1;
-        // Idle
+            if (this.grounded) {
+                this.velocity.y = -MAX_JUMP;
+                this.grounded = false;
+            }
         } else if (((!this.left && !this.right) || (this.left && this.right)) && !this.up) {
             this.state = 0;
             this.moving = 0;
             this.velocity.x = 0;
+        }
+
+        if (!this.grounded) {
+            if (this.velocity.y < 200) {
+                this.velocity.y += 10;
+            }
         }
         
         // if (!this.state == 1 && !this.state == 2) {
@@ -92,8 +104,15 @@ class player {
         //         this.velocity.x = MAX_RUN;
         //     }
         // }
-
+        if (!this.right && !this.left) {
+            if (this.velocity.y < 0) {
+                this.state = 2;
+            } else if (this.velocity.y > 0) {
+                this.state = 1;
+            }
+        }
         this.x += this.velocity.x * TICK;
+        this.y += this.velocity.y * TICK;
 
 
         //TODO collison bug
@@ -108,7 +127,7 @@ class player {
 
     updateBB() {
         this.lastBB = this.BB;
-        this.BB = new boundingbox(this.x + 3, this.y + 15, 47, 88, "Yellow");
+        this.BB = new boundingbox(this.x + 5, this.y + 12, 40, 90, "Yellow");
     }
 
 
@@ -120,7 +139,7 @@ class player {
             this.Xoffset = 0;
             this.Yoffset = 1;
         }
-        if (this.state == 2) {
+        if (this.state == 2 && this.moving == 0) {
             if (this.isIceGirl) {
                 this.Xoffset = 1;
                 this.Yoffset = 31;
@@ -165,7 +184,7 @@ class player {
 
         //TODO idle position is diff from running
         //to draw bounding box
-        // this.BB.draw(ctx)
+        // this.BB.draw(ctx);
 
         // this.animations[0].drawFrame(this.game.clockTick, ctx, 500, 500, .3);
         // this.animations[3].drawFrame(this.game.clockTick, ctx, 500  - 43, 500 + 42, .3);
@@ -176,35 +195,40 @@ class player {
     collisionCheck() {
         this.game.entities.forEach(entity => {
             if (entity.BB && this.BB.collide(entity.BB)) {
-                // Jumping
                 if (this.velocity.y > 0) {
                     // Landing on the ground
-                    if (entity instanceof Ground && this.lastBB.bottom <= entity.BB.top) {
-                        this.y = entity.BB.top - 51;
+                    this.grounded = false;
+                    if (entity instanceof Ground && this.lastBB.bottom < entity.BB.top && this.BB.collide(entity.topBB)) {
                         this.velocity.y = 0;
+                        this.grounded = true;
+                        console.log("Player has collided with the top of a block");
+                    } else if (entity instanceof Ground && this.BB.collide(entity.topBB)) {
+                        this.grounded = true;
                     }
-                }
-                // Falling
-                if (this.velocity.y < 0) {
+                // Jumping
+                } else if (this.velocity.y < 0) {
                     // Hitting ceiling
                     if (entity instanceof Ground && this.lastBB.top >= entity.BB.bottom) {
-                        this.y = entity.BB.top + 51;
+                        // this.y = entity.bottomBB.bottom - 2;
                         this.velocity.y = 0;
+                        console.log("Player has collided with the bottom of a block");
                     }
                 }
+                
                 // Collides with left side of the ground
-                if (entity instanceof Ground && this.BB.collide(entity.leftBB)) {
+                if (entity instanceof Ground && this.lastBB.right <= entity.BB.left && this.lastBB.bottom - 2 > entity.BB.top && this.BB.collide(entity.leftBB)) {
                     if (this.velocity.x > 0) {
                         this.velocity.x = 0;
-                    }
+                    }w
                     this.x = entity.leftBB.left - 50;
-                }
+                    console.log("Player has collided with the left side of a block");
                 // Collides with the right side of the ground
-                if (entity instanceof Ground && this.BB.collide(entity.rightBB)) {
+                } else if (entity instanceof Ground && this.lastBB.left >= entity.BB.right && this.lastBB.bottom - 2 > entity.BB.top && this.BB.collide(entity.rightBB)) {
                     if (this.velocity.x < 0) {
                         this.velocity.x = 0;
                     }
-                    this.x = entity.rightBB.right - 3;
+                    this.x = entity.rightBB.right - 2;
+                    console.log("Player has collided with the right side of a block");
                 }
             }
         });
