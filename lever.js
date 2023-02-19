363efbd4e7a46153871101a594e224a55a1a9bc4
Nathan Mahnke <nmahnke@uw.edu>
2023-02-18 19:51:33 -0800

    add rotating for the lever

--- lever.js
+++ lever.js
@@ -7,6 +7,7 @@ class lever {
         this.handle = ASSET_MANAGER.getAsset("./Assets/leverHandle.png");
         this.BB = new boundingbox(this.x, this.y, this.w, this.h, "Red");
         this.removeFromWorld = false;
+        this.currentAngle = 0;
     };
 
     update() {
@@ -14,7 +15,18 @@ class lever {
     };
 
     draw(ctx) {
-        ctx.drawImage(this.handle, this.x + this.w / 2 - 3, this.y + 0, 11, 32);
+        var offScreenCanvas = document.createElement('canvas');
+        offScreenCanvas.width = 64;
+        offScreenCanvas.height = 64;
+        var offScreenCtx = offScreenCanvas.getContext('2d');
+        offScreenCtx.save();
+        offScreenCtx.translate(32, 32);
+        offScreenCtx.rotate(Math.PI / 4);
+        offScreenCtx.translate(-32, -32);
+        offScreenCtx.drawImage(this.handle, 26.5, 0, 11, 32);
+        offScreenCtx.restore();
+
+        ctx.drawImage(offScreenCanvas, this.x + this.w / 2 - 35, this.y + 0, 64, 64);
         ctx.drawImage(this.base, this.x, this.y + (42 - this.h), this.w, this.h);
     }
 }
