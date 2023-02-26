69e881a64db964af9f7b402abb4595547c487348
Philip <philip1fox1@gmail.com>
2023-02-26 09:44:39 -0800

    retesting re-skin

--- ground.js
+++ ground.js
@@ -3,50 +3,50 @@ class ground {
         Object.assign(this, { game, x, y, side});
         this.h = 48
         this.w = 48
-        //this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/bricksOG.png");
+        //this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/bricksOG.png");
         // if (side == "0000") {
-        //     this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick0000.png");
+        //     this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick0000.png");
         // }
         switch (side) {
             case "A":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick0000.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick0000.png");
                 break;
             case "B":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick1111.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick1111.png");
                 break;
             case "C":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick0110.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick0110.png");
                 break;
             case "D":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick0111.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick0111.png");
                 break;
             case "E":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick1110.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick1110.png");
                 break;
             case "F":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick0010.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick0010.png");
                 break;
             case "G":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick0100.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick0100.png");
                 break;
             case "H":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick1000.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick1000.png");
                 break;
             case "I":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick0001.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick0001.png");
                 break;
             case "J":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick1100.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick1100.png");
                 break;
             case "K":
-                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Bricks/brick0101.png");
+                this.spritesheet = ASSET_MANAGER.getAsset("./Assets/bricks/brick0101.png");
                 break;
         }
         this.BB = new boundingbox(this.x + PARAMS.BLOCKWIDTH / 8, this.y, PARAMS.BLOCKWIDTH * 3 / 4, PARAMS.BLOCKWIDTH, "Red");
         this.leftBB = new boundingbox(this.x, this.y, PARAMS.BLOCKWIDTH / 2, PARAMS.BLOCKWIDTH, "Green");
         this.rightBB = new boundingbox(this.x + PARAMS.BLOCKWIDTH / 2, this.y, PARAMS.BLOCKWIDTH / 2, PARAMS.BLOCKWIDTH, "Blue");
         this.topBB = new boundingbox(this.x, this.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH / 2, "Blue");
-        this.bottomBB = new boundingbox(this.x, this.y + PARAMS.BLOCKWIDTH / 2, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH / 2, "Brown");
+        this.bottomBB = new boundingbox(this.x, this.y + PARAMS.BLOCKWIDTH / 2, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH / 2, "brown");
         this.removeFromWorld = false;
 
     };
